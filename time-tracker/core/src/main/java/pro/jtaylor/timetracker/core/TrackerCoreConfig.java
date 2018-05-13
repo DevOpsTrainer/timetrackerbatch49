abb0bb5d5e49eaf79ef73028b29254da9da4ccb7
DevOpsTrainer <mydevopstrainer@gmail.com>
2018-05-12 21:59:22 -0400

    Updated
    updated

--- time-tracker/core/src/main/java/pro/jtaylor/timetracker/core/TrackerCoreConfig.java
+++ time-tracker/core/src/main/java/pro/jtaylor/timetracker/core/TrackerCoreConfig.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Jason on 6/19/2015.
+ * Created by Jason on 05/12/2018.
  */
 @Configuration
 @ComponentScan("pro.jtaylor.timetracker.core")
